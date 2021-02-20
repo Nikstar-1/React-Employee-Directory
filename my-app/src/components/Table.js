f4c4d829c4bd5980dc8c1c8e2549ca2bee1a9fb9
Nikstar-1 <rk_787@hotmail.co.uk>
2021-02-20 20:15:20 +0000

    able to get important employee information from api onto the browser

--- my-app/src/components/Table.js
+++ my-app/src/components/Table.js
@@ -2,13 +2,28 @@ import React from "react";
 
 function Table({ employees }) {
   console.log(employees);
-  return employees.map(({ gender }) => {
+  return employees.map(({
+    location: { city, state, country, postcode },
+    picture: { thumbnail },
+    name: {title, first, last},
+     gender, email, cell, phone }) => {
     return (
         <>
       <div>
-    
+        <h3>{title}</h3>
+        <h3>{first}</h3>
+        <h3>{last}</h3>
+        <h3>{cell}</h3>
+        <h3>{phone}</h3>
         <h3>{gender}</h3>
-        
+        <h3>{email}</h3>
+        <h3>{city}</h3>
+        <h3>{state}</h3>
+        <h3>{country}</h3>
+        <h3>{postcode}</h3>
+
+        <img src={thumbnail} />
+
       </div>
       </>
     );

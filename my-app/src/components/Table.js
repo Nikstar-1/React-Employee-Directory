cc8db3b18b67dffc46f0fa9699980ad202429954
Nikstar-1 <rk_787@hotmail.co.uk>
2021-02-21 17:52:13 +0000

    managed to get employees into rows

--- my-app/src/components/Table.js
+++ my-app/src/components/Table.js
@@ -8,24 +8,40 @@ function Table({ employees }) {
     name: {title, first, last},
      gender, email, cell, phone }) => {
     return (
-        <>
-      <div>
-        <h3>{title}</h3>
-        <h3>{first}</h3>
-        <h3>{last}</h3>
-        <h3>{cell}</h3>
-        <h3>{phone}</h3>
-        <h3>{gender}</h3>
-        <h3>{email}</h3>
-        <h3>{city}</h3>
-        <h3>{state}</h3>
-        <h3>{country}</h3>
-        <h3>{postcode}</h3>
-
-        <img src={thumbnail} />
-
-      </div>
-      </>
+      <table>
+       <thead>
+        <tr>
+         <th>title</th>
+         <th>first</th>
+         <th>last</th>
+         <th>cell</th>
+         <th>phone</th>
+         <th>gender</th>
+         <th>email</th>
+         <th>city</th>
+         <th>state</th>
+         <th>country</th>
+         <th>postcode</th>
+        </tr>
+        </thead>   
+        <tbody>
+        <tr>
+          <td>{title}</td>
+          <td>{first}</td>
+          <td>{last}</td>
+          <td>{cell}</td>
+          <td>{phone}</td>
+          <td>{gender}</td>
+          <td>{email}</td>
+          <td>{city}</td>
+          <td>{state}</td>
+          <td>{country}</td>
+          <td>{postcode}</td>
+         <td><img src={thumbnail} /></td>
+         </tr>
+        </tbody>
+      </table>
+      
     );
   });
 }

750951fa178318b331d8baf2f196d4ff7d7995b7
Nikstar-1 <rk_787@hotmail.co.uk>
2021-02-21 19:59:28 +0000

    ask bcs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from "./components/Header";
-import Search from './components/search';
+import Search from './components/searchbar';
 import Main from "./components/Main";
 
 function App() {
